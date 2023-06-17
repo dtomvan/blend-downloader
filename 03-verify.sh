dcc9e142554db3054fd72c2263adccfe616286b1
Tom van Dijk <18gatenmaker6@gmail.com>
2023-06-18 01:46:58 +0200

    Add archive verification script

---
+++ 03-verify.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -eu
+
+tempfiles=()
+
+read -p 'Prefix > ' prefix
+
+evaluate() {
+    eval $1="$prefix"-"$1".zip
+    listvar=${1}_list
+    temp="$(mktemp)"
+    tempfiles+=("$temp")
+    eval $listvar="$temp"
+    eval zipinfo -1 "\$$1" > "$temp"
+    countvar=${1}_count
+    amount="$(cat $temp | wc -l)"
+    eval $countvar="$amount"
+}
+
+evaluate audio
+evaluate pdf
+
+expected_total="$(jq '. | values | flatten | length' metadata.json)"
+expected_pdf="$(jq '. | values | flatten[] | select(endswith("pdf"))' metadata.json | wc -l)"
+expected_audio=$(($expected_total - $expected_pdf))
+actual_total=$(($audio_count + $pdf_count))
+
+echo "Amount of audio files in the archive: $audio_count"
+echo "Amount of pdf files in the archive: $pdf_count"
+echo "Expected total amount of files: $expected_total"
+echo "Actual total amount of files: $actual_total"
+echo "Expected amount of pdf files: $expected_pdf"
+echo "Expected amount of audio files by deduction: $expected_audio"
+
+[ -z "$(comm -1 -2 ${tempfiles[@]:0:2} | tr -d '\n')" ] && echo "No overlapping files found in the archives." || echo "Overlapping files found between the two archives."
+
+grep blend-work ${tempfiles[@]} && echo "Files were referenced in blend-work. Archive will not extract as expected." || echo "No files references in blend-work/."
+
+for i in ${tempfiles[@]}; do
+    rm "$i"
+done
--- get-json.sh
+++
@@ -1,1 +0,0 @@
-curl -fsSL -H @headers.in https://blendassen.nl/ledenpagina/ | htmlq -- 'div.fusion-fullwidth:nth-child(2) > div:nth-child(1) > .fusion-layout-column > .fusion-column-wrapper' | pandoc -f html -t json --lua-filter convert-to-dict.lua | python pretty-json.py > metadata.json
