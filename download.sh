59ff16d964ee27e104d6ff2ba2dcfa6293e6e169
Tom van Dijk <18gatenmaker6@gmail.com>
2023-06-17 23:41:41 +0200

    fixup! Initial commit

--- download.sh
+++ download.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+set -eu
+
 work="./blend-work/"
 
 mkdir -p "$work"
 
 jq -r '. | to_entries | .[] | [.key]+.value | @tsv' metadata.json |
   while IFS=$'\n' read -r line; do
-      readarray -d $'\t' -t info <<< "$line"
+      readarray -d $'\t' -t info < <(printf '%s' "$line")
       target_dir="$work"/"${info[0]}"
       mkdir -p "$target_dir"
-      wget -P "$target_dir" "${info[@]:1}"
+      wget --random-wait -P "$target_dir" "${info[@]:1}"
+      sleep 1
   done
 
 dt="$(date -Iseconds)"
 cp -a "$work" blend-"$dt"/
 
 zip -r blend-"$dt".zip blend-"$dt"/
+
+rm -rf "$work"
