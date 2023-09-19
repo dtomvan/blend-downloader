5f4ef2b60dd813bd17886eda836a5156f6659aa2
Tom van Dijk <18gatenmaker6@gmail.com>
2023-09-19 22:20:59 +0200

    feat: also download christmas songs

--- 01-get-json.sh
+++ 01-get-json.sh
@@ -1,1 +1,15 @@
-curl -fsSL -H @headers.in https://blendassen.nl/ledenpagina/ | htmlq -- 'div.fusion-fullwidth:nth-child(3) > div:nth-child(1) > .fusion-layout-column > .fusion-column-wrapper' | pandoc -f html -t json --lua-filter utils/convert-to-dict.lua | python utils/pretty-json.py > metadata.json
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+query() {
+    printf "htmlq -- '%s'" "$@"
+}
+
+q_normal="div.fusion-fullwidth:nth-child(3) > div:nth-child(1) > .fusion-layout-column > .fusion-column-wrapper"
+q_christmas="div.fusion-fullwidth:nth-child(5) > div:nth-child(1) > .fusion-layout-column > .fusion-column-wrapper"
+
+curl -fsSL -H @headers.in https://blendassen.nl/ledenpagina/        \
+    | pee "$(query $q_normal)" "$(query $q_christmas)"              \
+    | pandoc -f html -t json --lua-filter utils/convert-to-dict.lua \
+    | python utils/pretty-json.py > metadata.json
