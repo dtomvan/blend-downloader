00d1cad8faa4532b8b3a6894ea7ded83c5456601
Tom van Dijk <18gatenmaker6@gmail.com>
2024-10-31 14:43:26 +0100

    fix: urls

--- 01-get-json.sh
+++ 01-get-json.sh
@@ -9,11 +9,11 @@ query() {
 declare -a metadata metadata_t
 metadata_t=(
     normal
-    # cristmas
+    christmas
 )
 mapfile -d '' metadata < <(printf "metadata-%s.json\0" "${metadata_t[@]}")
 q_normal="div.fusion-fullwidth:nth-child(2) > div:nth-child(1) > .fusion-layout-column > .fusion-column-wrapper"
-# q_christmas="div.fusion-fullwidth:nth-child(5) > div:nth-child(1) > .fusion-layout-column > .fusion-column-wrapper"
+q_christmas="div.fusion-fullwidth:nth-child(4) > div:nth-child(1) > .fusion-layout-column > .fusion-column-wrapper"
 
 do_query() {
     curl -fsSL -H @headers.in https://blendassen.nl/mp3s-en-bladmuziek/    \
